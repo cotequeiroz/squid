06966423646d32b6d84086a822168249637414f5
Eneas U de Queiroz <cote2004-github@yahoo.com>
2018-09-01 11:33:57 -0300

    Allow compilation with OPENSSL_NO_DEPRECATED
    Updated the calls to openssl deprecated API, so that squid can be
    compiled with openssl built with the OPENSSL_NO_DEPRECATED option.
    
    Most* of the calls in the main code were updated to the openssl 1.1 API,
    and the wrappers were moved into compat/openssl.h.
    * The exceptions to the rule above were: (a) the BIO table setup,
    which would need many calls, instead of a simple static struct;
    (b) library initialization, which is optional in openssl >= 1.1, and
    mandatory for < 1.1; (c) RSA key generation callback, which is absent in
    openssl >= 1.1.
    
    Applied changes from #265-Libressl support: Removed const from
    *useMethod in src/ssl/bio.cc
    
    Signed-off-by: Eneas U de Queiroz <cote2004-github@yahoo.com>

--- src/ssl/bio.cc
+++ src/ssl/bio.cc
@@ -76,7 +76,7 @@ Ssl::Bio::Create(const int fd, Security::Io::Type type)
         BIO_meth_set_create(SquidMethods, squid_bio_create);
         BIO_meth_set_destroy(SquidMethods, squid_bio_destroy);
     }
-    const BIO_METHOD *useMethod = SquidMethods;
+    BIO_METHOD *useMethod = SquidMethods;
 #else
     BIO_METHOD *useMethod = &SquidMethods;
 #endif
@@ -706,13 +706,7 @@ applyTlsDetailsToSSL(SSL *ssl, Security::TlsDetails::Pointer const &details, Ssl
             cbytes[0] = (cipherId >> 8) & 0xFF;
             cbytes[1] = cipherId & 0xFF;
             cbytes[2] = 0;
-#if HAVE_LIBSSL_SSL_CIPHER_FIND
-            const SSL_CIPHER *c = SSL_CIPHER_find(ssl, cbytes);
-#else
-            const SSL_METHOD *method = SSLv23_method();
-            const SSL_CIPHER *c = method->get_cipher_by_char(cbytes);
-#endif
-            if (c != NULL) {
+            if (auto c = SSL_CIPHER_find(ssl, cbytes)) {
                 if (!strCiphers.isEmpty())
                     strCiphers.append(":");
                 strCiphers.append(SSL_CIPHER_get_name(c));
